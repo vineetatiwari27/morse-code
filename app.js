8b247c5726acc52a453df4294f3ebd9b7761cd19
vineetatiwari27 <85996556+vineetatiwari27@users.noreply.github.com>
2021-09-23 00:40:01 +0530

    docs: changes

---
+++ app.js
@@ -0,0 +1,24 @@
+var btInput = document.querySelector("#textInput");
+var inputText = document.querySelector("#txtInput");
+var outputText = document.querySelector("#txtOutput")
+
+var serverUrl = "http://api.funtranslations.com/translate/morse.json"
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! Try again later :(")
+}
+function clickHandler() {
+    var data = inputText.value;
+    fetch(getTranslationUrl(data))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText;
+        })
+        .catch(errorHandler);
+
+};
+btInput.addEventListener("click", clickHandler)
